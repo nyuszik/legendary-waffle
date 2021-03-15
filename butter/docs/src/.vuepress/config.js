946984fa8a1cde63c45cc7f94580329aacb83eeb
félénk nyúszika <felenk@outlook.hu>
2021-03-15 12:21:06 -0400

    hero image

---
+++ butter/docs/src/.vuepress/config.js
@@ -0,0 +1,92 @@
+const { description } = require('../../package');
+const path = require("path");
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Open Source Unicorn',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: 'Kaska Miskolczi, Perl, Open Source, Designer, Developer, CTO, CISO, Serverless, Azure',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  base: "/legendary-waffle/",
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    smoothScroll: true,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      },
+      {
+        text: 'VuePress',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+    ['@vuepress/search', {
+      searchMaxSuggestions: 10
+    }],
+    [
+      'vuepress-plugin-clean-urls',
+      {
+        normalSuffix: '/',
+        indexSuffix: '/',
+        notFoundPath: '/404.html',
+      },
+    ]
+  ],
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@assets': path.resolve(__dirname,"../assets")
+      }
+    }
+  }
+}
